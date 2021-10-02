4912e33ca1953fca950202487ffc02a5217bda87
github-action-benchmark <github@users.noreply.github.com>
2021-10-02 01:32:52 +0000

    add Catch2 Benchmark (catch2) benchmark result for 30c3fadef598c25ba483d94dffdb58eb59d7a7d9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,7 +1,38 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1629032908138,
+  "lastUpdate": 1633138372745,
   "entries": {
     "Catch2 Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "hyoklee@hdfgroup.org",
+            "name": "H. Joe Lee",
+            "username": "hyoklee"
+          },
+          "committer": {
+            "email": "hyoklee@hdfgroup.org",
+            "name": "H. Joe Lee",
+            "username": "hyoklee"
+          },
+          "distinct": true,
+          "id": "30c3fadef598c25ba483d94dffdb58eb59d7a7d9",
+          "message": "OESS-156: turn off outputs for sync case.",
+          "timestamp": "2021-10-01T20:26:21-05:00",
+          "tree_id": "b85be66111cb2e8735fb7b21fbec9f191ae7574c",
+          "url": "https://github.com/hyoklee/vol-tests/commit/30c3fadef598c25ba483d94dffdb58eb59d7a7d9"
+        },
+        "date": 1633138370956,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "vol_test 100",
+            "value": 10,
+            "range": "± 0",
+            "unit": "ns",
+            "extra": "3 samples\n2969 iterations"
+          }
+        ]
+      }
     ]
   },
   "repoUrl": "https://github.com/hyoklee/vol-tests"
